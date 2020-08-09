9b23eb64a43fdc1e90724f3f1e963e0786da8940
MateuszMichalik93 <59762524+MateuszMichalik93@users.noreply.github.com>
2020-08-09 11:31:53 +0200

    v4 commit

---
+++ src/main/java/com/example/blog/service/BlogServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.blog.service;
+
+import com.example.blog.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BlogServiceImpl implements BlogService{
+    @Override
+    public boolean addUser(User user) {
+
+        return false;
+    }
+
+    @Override
+    public boolean deleteUser(long userId) {
+        return false;
+    }
+
+    @Override
+    public boolean updatePassword(long userId, String newPassword) {
+        return false;
+    }
+
+    @Override
+    public List<User> getAllUsersOrderByRegistrationDateDesc() {
+        return null;
+    }
+
+    @Override
+    public Optional<User> getUserById(long id) {
+        return Optional.empty();
+    }
+}
