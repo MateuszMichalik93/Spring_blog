83856c508178d9d20fa0059d478bada53da9341f
MateuszMichalik93 <59762524+MateuszMichalik93@users.noreply.github.com>
2020-08-09 11:26:08 +0200

    third commit

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -2,12 +2,20 @@ package com.example.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
+    //żądanie dodania nowego użytkownika do tabeli user
+    @PostMapping("/addUser")
+    public boolean addUser(){
 
+        return false;
+    }
 }
---
+++ src/main/java/com/example/blog/service/BlogService.java
@@ -0,0 +1,20 @@
+package com.example.blog.service;
+
+import com.example.blog.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BlogService {
+    //dodawanie użytkownika
+        boolean addUser(User user);
+    //usuwanie użytkownika
+        boolean deleteUser(long userId);
+    //zmiana hasla
+        boolean updatePassword(long userId, String newPassword);
+    //pobranie wszystkich użytkowników
+        List<User> getAllUsersOrderByRegistrationDateDesc();
+    //pobranie użytkownika po id
+    Optional<User> getUserById(long id);
+
+}
